fc91db08f5f206c5dc5af6003f97f9705aada3db
p4535992 <tentimarco0@gmail.com>
2023-04-23 16:07:03 +0200

    version 3.2.1

--- src/scripts/components/ChatResolver.ts
+++ src/scripts/components/ChatResolver.ts
@@ -207,7 +207,7 @@ export class ChatResolver {
 
 	static onRenderChatMessage(chatMessage, html, messageData) {
 		if (!messageData.message.flags) {
-			if ($(chatMessage.content).find(".chat-media-image")) {
+			if (chatMessage?.content && $(chatMessage.content).find(".chat-media-image")) {
 				messageData.message.flags ??= {};
 				messageData.message.flags["chat-media"] = { subType: ChatResolver.CHAT_MESSAGE_SUB_TYPES.CIMAGE };
 
